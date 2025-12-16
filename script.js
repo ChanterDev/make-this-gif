84a1c30cf3fe08bdf32d84942ce0da31862701b4
ChanterDev <matvey.dibrov@yandex.ru>
2025-12-17 02:15:50 +0300

    added main struct and properties

--- script.js
+++ script.js
@@ -1,54 +1,45 @@
 let url;
 let link;
 let image;
+let compress;
+
+function clearWhenPossible() {
+    image.value = "";
+    setTimeout(() => {
+        link.href = "";
+        link.removeAttribute('download');
+    }, 100);
+}
 
 window.onload = () => {
     link = document.getElementById("download-link");
     image = document.getElementById("image");
+    compress = document.getElementById("compress");
     image.addEventListener("change", () => {
         if (image.files.length === 1) {
-            if (link.href !== undefined && link.href !== "" && isValidUrl(link.href))
-                URL.revokeObjectURL(link.href);
-            var img = new Image();
-            img.onload = convert;
-            img.src = URL.createObjectURL(image.files[0]);
-        } else {
-            if (link.href !== undefined && link.href !== "" && isValidUrl(link.href))
-                URL.revokeObjectURL(link.href);
+            convertToFakeGIF(image.files[0]);
         }
     });
+    link.addEventListener("click", clearWhenPossible);
 };
 
-function isValidUrl(string) {
-    try {
-        new URL(string);
-        return true;
-    } catch (error) {
-        return false;
-    }
-}
+function convertToFakeGIF(file) {
+    var img = new Image();
+    img.onload = () => {
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
 
-function convert() {
-    URL.revokeObjectURL(this.src); // Free up memory
-    var canvas = document.createElement("canvas");
-    var ctx = canvas.getContext("2d");
+        canvas.width = img.width;
+        canvas.height = img.height;
 
-    // Set canvas dimensions to the image's
-    canvas.width = this.width;
-    canvas.height = this.height;
+        ctx.drawImage(img, 0, 0);
 
-    // Draw the image onto the canvas
-    ctx.drawImage(this, 0, 0);
-
-    // Convert canvas to a Blob (File-like object) with JPEG format and quality
-    // 'quality' can be between 0 and 1, default is 0.92
-    canvas.toBlob(function(blob) {
-        // You can now use the 'blob' object, e.g., upload it or trigger a download
-        var file = new File([blob], "converted.gif", { type: "image/gif" });
-        
-        // Example of triggering a download link:
-        link.href = URL.createObjectURL(file);
-        link.download = 'converted.gif';
-        
-    }, "image/jpeg", 0.8); // Specify JPEG mime-type and quality (0.8 = 80%)
+        canvas.toBlob((blob) => {
+            var file = new File([blob], "converted.gif", { type: "image/gif" });
+            link.href = URL.createObjectURL(file);
+            link.download = 'converted.gif';
+        }, "image/jpeg", parseInt(compress.value) / 100);
+        URL.revokeObjectURL(img.src); // Free up memory
+    };
+    img.src = URL.createObjectURL(file);
 }
